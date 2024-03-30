6867ada2fb8fec319322f8af48f2bb0e49067139
mys1erious <subtletyzeros@gmail.com>
2024-03-30 12:45:43 +0100

    feat: add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
     return (
         <main>
-          <h1 className="text-3xl font-bold underline">
-            Portal
-          </h1>
+            <h1 className='text-3xl font-bold underline'>Portal</h1>
         </main>
     );
 }
